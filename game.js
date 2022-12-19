aa76663940d6bc32c8b84296f86e84fabc763df4
Santiago Dubon <santiagodubon@outlook.com>
2022-12-18 20:37:51 -0500

    Added current player tracking

--- game.js
+++ game.js
@@ -1,3 +1,14 @@
+// ### FACTORIES ###
+const Player = (id, symbol) => {
+    const getId = () => id
+
+    const getSymbol = () => {
+        return symbol
+    }
+
+    return { getId, getSymbol }
+}
+
 // ### MODULES ###
 const displayController = (() => {
     /**
@@ -7,6 +18,13 @@ const displayController = (() => {
      */
     const createBoard = (board) => {
         console.log("BOARD: " + board)
+
+        gameController.trackCurrPlayer()
+        const currentPlayer = gameController.getCurrentPlayer()
+
+        console.log("CURRENT PLAYER SYMBOL: " + currentPlayer.getSymbol())
+        console.log("CURRENT PLAYER: " + currentPlayer)
+
         const container = document.getElementById('game-board')
 
         // Loop through rows
@@ -18,7 +36,7 @@ const displayController = (() => {
                 const tile = document.createElement('div')
                 tile.innerHTML = e
                 tile.addEventListener('click', function() {
-                    gameController.chooseTile(x, y)
+                    gameController.chooseTile(x, y, currentPlayer)
                 })
 
                 row.appendChild(tile)
@@ -50,27 +68,47 @@ const gameBoard = (() => {
 })()
 
 const gameController = (() => {
+    const playerOne = Player(1, 'X')
+    const playerTwo = Player(2, 'O')
+
+    let currentPlayer = ''
+
     /**
      * Handles player selecting a tile
      */
-    const chooseTile = (x, y) => {
+    const chooseTile = (x, y, currentPlayer) => {
         const board = gameBoard.getBoard()
-        board[x][y] = 'X'
+        board[x][y] = currentPlayer.getSymbol()
 
         gameBoard.setBoard(board)
         displayController.createBoard(gameBoard.getBoard())
     }
 
-    return { chooseTile }
+    /**
+     * Tracks and updates the current player
+     * 
+     * @param {Player} playerOne 
+     * @param {Player} playerTwo 
+     */
+    const trackCurrPlayer = () => {
+        if (currentPlayer == '') {
+            setCurrentPlayer(playerOne)
+        } else if (currentPlayer.getSymbol() == 'X') {
+            setCurrentPlayer(playerTwo)
+        } else if (currentPlayer.getSymbol() == 'O') {
+            setCurrentPlayer(playerOne)
+        }
+    }
+
+    const setCurrentPlayer = (player) => {
+        currentPlayer = player
+    }
+
+    const getCurrentPlayer = () => {
+        return currentPlayer
+    }
+
+    return { chooseTile, trackCurrPlayer, getCurrentPlayer }
 })()
-
-// ### FACTORIES ###
-const Player = (id, symbol) => {
-    const getId = () => id
-
-    const getSymbol = () => symbol
-
-    return { id, symbol, getId, getSymbol }
-}
 
 displayController.createBoard(gameBoard.getBoard())
